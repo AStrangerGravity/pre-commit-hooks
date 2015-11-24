15662eb901f34fd33065ea478105578dc502478f
John Austin <kleptine@gmail.com>
2015-11-24 00:40:15 -0800

    Uses a much better way to judge whether a file is text.

--- pre_commit_hooks/utils.py
+++ pre_commit_hooks/utils.py
@@ -4,26 +4,20 @@ from __future__ import unicode_literals
 
 import string
 import subprocess
-
-# Taken from: http://code.activestate.com/recipes/173220-test-if-a-file-or-string-is-text-or-binary/
-
-KNOWN_BINARY_FILE_EXT = ['.pdf']
-ALLOWED_NON_PRINTABLE_THRESHOLD = 0.15
+import magic
 
 def is_textfile(filename, blocksize=512):
-    if any(filename.endswith(ext) for ext in KNOWN_BINARY_FILE_EXT):
-        return False
-    return is_text(open(filename).read(blocksize))
+    f = magic.Magic(mime=True, uncompress=True)
+    mime = f.from_file(filename)
 
-def is_text(stuff):
-    if "\0" in stuff:
-        return False
-    if not stuff:  # Empty files are considered text
-        return True
-    # Get the non-text characters (maps a character to itself then
-    # use the 'remove' option to get rid of the text characters.)
-    non_printable_chars = ''.join(c for c in stuff if c not in string.printable)
-    return len(non_printable_chars) / len(stuff) < ALLOWED_NON_PRINTABLE_THRESHOLD
+    # empty files are text
+    if mime == "inode/x-empty":
+      return True
+
+    if "text/" in mime:
+      return True
+
+    return false
 
 class CalledProcessError(RuntimeError):
     pass
